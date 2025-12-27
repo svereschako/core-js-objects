557353c8479343039ca70a336fc54d2751dc2800
svereschako <33933400+svereschako@users.noreply.github.com>
2025-12-27 04:28:46 +0200

    fix: amend solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,9 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(obj) {
-  const copy = {};
-  return Object.assign(copy, obj);
+function shallowCopy( obj ) {
+  //throw new Error('Not implemented');
+  return Object.assign({}, obj);
 }
 
 /**
@@ -33,14 +33,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(objects) {
-  return objects
-    .map(Object.entries)
-    .flat()
-    .reduce((acc, [key, val]) => {
-      acc[key] = (acc[key] || 0) + val;
-      return acc;
-    }, {});
+function mergeObjects( objects ) {
+  //throw new Error('Not implemented');
+  let res = {};
+  objects.forEach(el => {    
+    for(let [key, value] of Object.entries(el)){
+      if(!res[key])
+        res[key] = value;
+      else
+        res[key] += value;
+    }
+  });
+  return res;
 }
 
 /**
@@ -56,19 +60,32 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(obj, keys) {
-  const copy = { ...obj };
-
-  if (!keys) {
-    return obj;
-  }
-
-  keys.forEach((key) => {
-    if (Object.prototype.hasOwnProperty.call(copy, key)) {
-      delete copy[key];
-    }
+function removeProperties( obj, keys ) {
+  //throw new Error('Not implemented');
+  if(typeof keys === "string")
+    keys = [keys];
+  keys.forEach(k => {
+    if(obj[k])
+      delete obj[k];
   });
-  return copy;
+  return obj;
+}
+
+/**
+ * Compares two source objects. Returns true if the objects are equal and false otherwise.
+ * There are no nested objects.
+ *
+ * @param {Object} obj1 - The first object to compare
+ * @param {Object} obj2 - The second object to compare
+ * @return {boolean} - True if the objects are equal, false otherwise
+ *
+ * @example
+ *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
+ *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
+ */
+function compareObjects( obj1, obj2 ) {
+  //throw new Error('Not implemented');
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -82,7 +99,8 @@ function removeProperties(obj, keys) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(obj) {
+function isEmptyObject( obj ) {
+  //throw new Error('Not implemented');  
   return Object.keys(obj).length === 0;
 }
 
@@ -102,7 +120,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(obj) {
+function makeImmutable( obj ) {
+  //throw new Error('Not implemented');
   return Object.freeze(obj);
 }
 
@@ -116,16 +135,13 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(lettersObject) {
-  return Object.entries(lettersObject)
-    .reduce((word, [letter, positions]) => {
-      positions.forEach((position) => {
-        const copyWord = word;
-        copyWord[position] = letter;
-      });
-      return word;
-    }, [])
-    .join('');
+function makeWord( lettersObject ) {
+  //throw new Error('Not implemented');
+  res = [];
+  for(let p in lettersObject){
+    lettersObject[p].forEach(el => res[el] = p);
+  }
+  return res.join("");
 }
 
 /**
@@ -142,29 +158,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(queue) {
-  let [bill25, bill50] = [0, 0];
-
-  return queue.reduce((changeAvailable, bill) => {
-    if (!changeAvailable) return false;
-
-    if (bill === 25) {
-      bill25 += 1;
-      return true;
+function sellTickets( queue ) {
+  //throw new Error('Not implemented');
+  let change = 0;
+  for(let i=0;i<queue.length;i++){
+    if(queue[i] - 25 > change){
+      //break;
+      return false;
     }
-    if (bill === 50) {
-      bill25 -= 1;
-      bill50 += 1;
-    } else if (bill === 100) {
-      if (bill50 > 0) {
-        bill50 -= 1;
-        bill25 -= 1;
-      } else {
-        bill25 -= 3;
-      }
-    }
-    return bill25 >= 0;
-  }, true);
+    change += 25;
+  }
+  return true;
 }
 
 /**
@@ -180,11 +184,15 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  this.width = width;
-  this.height = height;
-
-  this.getArea = () => this.width * this.height;
+function Rectangle( width, height ) {
+  //throw new Error('Not implemented');
+  return {
+    width: width,
+    height: height,
+    getArea: function() {
+      return this.width * this.height;
+    }
+  };
 }
 
 /**
@@ -197,26 +205,11 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(obj) {
+function getJSON( obj ) {
+  //throw new Error('Not implemented');
   return JSON.stringify(obj);
 }
 
-/**
- * Compares two source objects. Returns true if the objects are equal and false otherwise.
- * There are no nested objects.
- *
- * @param {Object} obj1 - The first object to compare
- * @param {Object} obj2 - The second object to compare
- * @return {boolean} - True if the objects are equal, false otherwise
- *
- * @example
- *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
- *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
- */
-function compareObjects(obj1, obj2) {
-  // That's fun :)
-  return getJSON(obj1) === getJSON(obj2);
-}
 /**
  * Returns the object of specified type from JSON representation
  *
@@ -228,8 +221,11 @@ function compareObjects(obj1, obj2) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(proto, json) {
-  return Object.assign(Object.create(proto), JSON.parse(json));
+function fromJSON( proto, json ) {
+  //throw new Error('Not implemented');
+  let obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+  return obj;
 }
 
 /**
@@ -258,14 +254,24 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(arr) {
+function sortCitiesArray( arr ) {
+  //throw new Error('Not implemented');
   return arr.sort((a, b) => {
-    if (a.country === b.country) {
-      return a.city.localeCompare(b.city);
+    if(a.country > b.country)
+      return 1;
+    else if(a.country < b.country)
+      return -1;
+    else{
+      if(a.city > b.city)
+        return 1;
+      else if(a.city < b.city)
+        return -1;
+      else
+        return 0;
     }
-    return a.country.localeCompare(b.country);
   });
 }
+
 /**
  * Groups elements of the specified array by key.
  * Returns multimap of keys extracted from array elements via keySelector callback
@@ -296,21 +302,23 @@ function sortCitiesArray(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(array, keySelector, valueSelector) {
-  const resultMap = new Map();
-
-  array.forEach((item) => {
-    const key = keySelector(item);
-    const value = valueSelector(item);
-
-    if (!resultMap.has(key)) {
-      resultMap.set(key, []);
+function group( array, keySelector, valueSelector ) {
+  //throw new Error('Not implemented');
+  let countries = array.map(keySelector);
+  let cities = array.map(valueSelector);
+  let res = {};
+  //countries.filter((el, ind, arr) => arr.indeOf(el) === ind);
+  cities.forEach(el => {
+    for(let i=0;i<array.length;i++){
+      if(el === array[i].city){
+        if(!res[array[i].country])
+          res[array[i].country] = [];
+        res[array[i].country].push(el);
+        break;
+      }
     }
-
-    resultMap.get(key).push(value);
   });
-
-  return resultMap;
+  return new Map(Object.entries(res));
 }
 
 /**
@@ -366,35 +374,221 @@ function group(array, keySelector, valueSelector) {
  *
  *  For more examples see unit tests.
  */
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element( value ) {
+    //throw new Error('Not implemented');
+    //elementSelector(value);
+    //return Object.create(cssSelectorBuilder);
+    return new elementSelector(value);    
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id( value ) {
+    //throw new Error('Not implemented');
+    //idSelector(value);
+    //return Object.create(cssSelectorBuilder);
+    return new idSelector(value);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class( value ) {
+    //throw new Error('Not implemented');
+    //classSelector(value);
+    //return Object.create(cssSelectorBuilder);
+    return new classSelector(value);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr( value ) {
+    //throw new Error('Not implemented');
+    //attrSelector(value);
+    //return Object.create(cssSelectorBuilder);
+    return new attrSelector(value);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass( value ) {
+    //throw new Error('Not implemented');
+    //pseudoClassSelector(value);
+    //return Object.create(cssSelectorBuilder);
+    return new pseudoClassSelector(value);
   },
 
   pseudoElement(/* value */) {
     throw new Error('Not implemented');
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine( selector1, combinator, selector2 ) {
+    //throw new Error('Not implemented');
+  //const obj = new combineSelector(selector1, combinator, selector2);
+  //return Object.setPrototypeOf(obj, pr);
+  //selector1 = selector1.stringify();
+  //selector2 = selector2.stringify();
+  return new combineSelector(selector1, combinator, selector2);
   },
 };
+
+cssSelectorBuilder.val = [];
+cssSelectorBuilder.isLast = false;
+cssSelectorBuilder.isLst = false;
+cssSelectorBuilder.res = "";
+cssSelectorBuilder.stringify = function(){
+  cssSelectorBuilder.isLast = true;  
+  return cssSelectorBuilder.val.join("");
+};
+
+function elementSelector(value) {
+  if(cssSelectorBuilder.isLast){
+    cssSelectorBuilder.val = [];
+    cssSelectorBuilder.isLast = false;
+  }
+  this.val.push(value);
+  this.stringify = function() {
+    return this.val.join("");
+  };  
+  //cssSelectorBuilder.val.push(value);
+  //console.log(Object.getPrototypeOf(this));
+  //Object.getPrototypeOf(this).val.push(value);
+  const obj = {stringify: function(){return this.val.join("")}};
+  //return obj;
+  //return Object.setPrototypeOf(obj, cssSelectorBuilder);  
+}
+
+function idSelector(value) {
+  if(cssSelectorBuilder.isLast){
+    cssSelectorBuilder.val = [];
+    cssSelectorBuilder.isLast = false;
+  }
+  this.val.push("#" + value);
+  this.stringify = function() {
+    return this.val.join("");
+  };
+  //cssSelectorBuilder.val.push("#" + value);
+  //Object.getPrototypeOf(this).val.push("#" + value);
+  const obj = {stringify: function(){return this.val.join("")}};
+  //return obj;
+  //return Object.setPrototypeOf(obj, cssSelectorBuilder);  
+}
+
+function classSelector(value) {
+  if(cssSelectorBuilder.isLast){
+    cssSelectorBuilder.val = [];
+    cssSelectorBuilder.isLast = false;
+  }
+  this.val.push("." + value);
+  this.stringify = function() {
+    return this.val.join("");
+  };
+  //cssSelectorBuilder.val.push("." + value);
+  //Object.getPrototypeOf(this).val.push("." + value);
+  const obj = {stringify: function(){return this.val.join("")}};
+  //return obj;
+  //return Object.setPrototypeOf(obj, cssSelectorBuilder);  
+}
+
+function attrSelector(value) {
+  if(cssSelectorBuilder.isLast){
+    cssSelectorBuilder.val = [];
+    cssSelectorBuilder.isLast = false;
+  }
+  this.val.push("[" + value + "]");
+  this.stringify = function() {
+    return this.val.join("");
+  };
+  //cssSelectorBuilder.val.push("[" + value + "]");
+  //Object.getPrototypeOf(this).val.push("[" + value + "]");
+  const obj = {stringify: function(){return this.val.join("")}};
+  //return obj;
+  //return Object.setPrototypeOf(obj, cssSelectorBuilder);  
+}
+
+function pseudoClassSelector(value) {
+  if(cssSelectorBuilder.isLast){
+    cssSelectorBuilder.val = [];
+    cssSelectorBuilder.isLast = false;
+  }
+  this.val.push(":" + value);
+  this.stringify = function() {
+    return this.val.join("");
+  };
+  //cssSelectorBuilder.val.push(":" + value);
+  //Object.getPrototypeOf(this).val.push(":" + value);
+  const obj = {stringify: function(){return this.val.join("")}};
+  //return obj;
+  //return Object.setPrototypeOf(obj, cssSelectorBuilder);  
+}
+
+function combineSelector(sel1, comb, sel2) {
+  this.val = "";
+  elementSelector.prototype.val = this.val;
+  idSelector.prototype.val = this.val;
+  classSelector.prototype.val = this.val;
+  attrSelector.prototype.val = this.val;
+  pseudoClassSelector.prototype.val = this.val;
+  //if(obj.isLast){
+    //cssSelectorBuilder.val = [];
+    //obj.isLast = false;
+    //obj.sel1 = "";
+    //obj.sel2 = "";
+  //}  
+  //this.sel1 = []; 
+  //this.sel1.push(sel1.stringify());
+  //cssSelectorBuilder.val.push(sel1.stringify());
+  /*this.isLast = false;
+  this.sel1 = sel1.stringify();  
+  this.comb = comb;
+  this.sel2 = sel2.stringify();*/
+  //this.sel2 = [];
+  //this.sel2.push(sel2.stringify());
+  //cssSelectorBuilder.val.push(sel2.stringify());
+  /*this.stringify = function() {
+    cssSelectorBuilder.isLast = true;    
+    return this.sel1 + " " + comb + " " + this.sel2;
+  }; */
+  /*this.stringify = function() {
+    this.isLast = true;
+    return this.sel1 + " " + this.comb + " " + this.sel2;
+  };*/
+  //if(!res)
+    //var res = "";
+  const obj = {isLast: false, getComb: function() {return comb;}, stringify: function() {
+    //debugger;
+    let res = "";
+    if(cssSelectorBuilder.isLst){
+      //cssSelectorBuilder.val.splice(-1, 3);
+      cssSelectorBuilder.isLst = false;
+      //obj.sel1 = "";
+      //obj.sel2 = "";
+      //cssSelectorBuilder.res = "";
+      //cssSelectorBuilder.res = [];
+      //return cssSelectorBuilder.res;
+      //res = "";
+    }  
+    cssSelectorBuilder.isLst = true;    
+    res += sel1.stringify();
+    res += comb;
+    res += sel2.stringify();
+    //console.log(res);    
+    return res;
+    //debugger;
+    //let m = obj.sel1 + " " + obj.getComb() + " " + obj.sel2;
+    //console.log(m);
+    //return obj.sel1 + " " + obj.getComb() + " " + obj.sel2;
+  }};
+  
+  return obj;
+  //return Object.setPrototypeOf(obj, pr); 
+}
+const pr = {
+  stringify: function() {
+    //cssSelectorBuilder.isLast = true;
+    this.isLast = true;    
+    return this.sel1 + " " + this.comb + " " + this.sel2;
+    //return cssSelectorBuilder.val.join("");
+  }
+};
+//elementSelector.prototype = cssSelectorBuilder;
+//idSelector.prototype = cssSelectorBuilder;
+//classSelector.prototype = cssSelectorBuilder;
+//attrSelector.prototype = cssSelectorBuilder;
+//pseudoClassSelector.prototype = cssSelectorBuilder;
 
 module.exports = {
   shallowCopy,
